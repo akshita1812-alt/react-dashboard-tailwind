1a608088ccbd3d063bbf01836fee565d04b93ea6
Akshita <Akshita@Akshitas-MacBook-Pro.local>
2023-10-20 19:28:26 +0530

    changes class to classNAme

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ContextProvider } from './contexts/ContextProvider';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
+     {/* <BrowserRouter> */}
     <ContextProvider>
     <App />
     </ContextProvider>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
